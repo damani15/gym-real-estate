9036947f95ed5c8b8669676fbd2e4a0ee4716ce9
ArmCode22 <81496048+ArmCode22@users.noreply.github.com>
2021-07-17 16:05:08 -0400

    Update __init__.py

--- gym-real-estate/__init__.py
+++ gym-real-estate/__init__.py
@@ -2,5 +2,5 @@ from gym.envs.registration import register
 
 register(
     id='gym-real-estate-v0',
-    entry_point='gym_real-estate.envs:Real-EstateEnv',
+    entry_point='gym-real-estate.envs:RealEstateEnv',
 )
